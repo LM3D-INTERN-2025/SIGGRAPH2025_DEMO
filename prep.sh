ad7d5dad348811ec0282d305e4802a8ab3d73387
Gannub <gannubvv@gmail.com>
2025-07-30 10:42:13 +0700

    minor output dir changes

--- prep.sh
+++ prep.sh
@@ -2,13 +2,19 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 
-OUT_NAME="bbs_f1"
-GA_NAME="ga_bbs_f1"
+# let user input the name of the output
+echo "Please enter the name of the output (default: date_time):"
+read OUT_NAME
+if [ -z "$OUT_NAME" ]; then
+    echo "No name provided, using default: date_time"
+    OUT_NAME="$(date +%Y%m%d_%H%M%S)"
+fi
+GA_NAME="ga_$OUT_NAME"
 
 # Prepare for nerf exporting
 echo "Preparing for nerf exporting..."
 conda activate lumio
-python Prep/convert.py -i /mnt/nas/sitt/demo/data_prep/lumio_scans/bird_blue_shirt/ -o data/to_export/$OUT_NAME -itype flash_1
+python Prep/convert.py -i /mnt/nas/sitt/demo/data_prep/lumio_scans/mj/ -o data/to_export/$OUT_NAME -itype flash_1
 conda deactivate
 
 # Prepare for Gaussian Avatar
@@ -22,5 +28,5 @@ conda deactivate
 conda activate gaussian-avatars
 echo "Training Gaussian Avatar..."
 cd GaussianAvatars
-python train.py --bind_to_mesh --port 60030 --interval_media 100 --depth --interval 1000 --sh_degree 3 -s ../data/to_ga/$OUT_NAME --bcull -m output/pipe/$GA_NAME
+python train.py --bind_to_mesh --port 60030 --interval_media 100 --depth --interval 1000 --sh_degree 3 -s ../data/to_ga/$OUT_NAME --bcull -m ../result/pipe/$GA_NAME
 conda deactivate
